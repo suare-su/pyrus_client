75174b087e703c6dd4696b486d0cf5c43d2db1da
marvin255 <postbox433@gmail.com>
2024-06-09 19:38:24 +0200

    Fix

--- src/Client/PyrusClientOptions.php
+++ src/Client/PyrusClientOptions.php
@@ -11,6 +11,10 @@ use SuareSu\PyrusClient\Pyrus\PyrusBaseUrl;
  */
 final class PyrusClientOptions
 {
+    public const DEFAULT_TIMEOUT = 15;
+    public const DEFAULT_RETRIES = 3;
+    public const DEFAULT_RETRY_TIMEOUT = 5;
+
     /** @psalm-var non-empty-string */
     public readonly string $accountsBaseUrl;
 
@@ -19,7 +23,10 @@ final class PyrusClientOptions
      */
     public function __construct(
         /* @psalm-var non-empty-string|PyrusBaseUrl */
-        string|PyrusBaseUrl $accountsBaseUrl = PyrusBaseUrl::ACCOUNTS
+        string|PyrusBaseUrl $accountsBaseUrl = PyrusBaseUrl::ACCOUNTS,
+        public readonly int $timeout = self::DEFAULT_TIMEOUT,
+        public readonly int $retries = self::DEFAULT_RETRIES,
+        public readonly int $retryTimeout = self::DEFAULT_RETRY_TIMEOUT,
     ) {
         $this->accountsBaseUrl = $accountsBaseUrl instanceof PyrusBaseUrl ? $accountsBaseUrl->value : $accountsBaseUrl;
     }
