1fb04456d184b3fcadac98351b65936bae90a516
marvin255 <postbox433@gmail.com>
2024-12-14 11:44:26 +0100

    Add client getter to gateway interface

--- src/Gateway/PyrusGatewayImpl.php
+++ src/Gateway/PyrusGatewayImpl.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace SuareSu\PyrusClient\Gateway;
 
-use SuareSu\PyrusClient\Client\PyrusAuthToken;
 use SuareSu\PyrusClient\Client\PyrusClient;
-use SuareSu\PyrusClient\Client\PyrusCredentials;
 use SuareSu\PyrusClient\DataConverter\PyrusDataConverter;
 use SuareSu\PyrusClient\Entity\Catalog\Catalog;
 use SuareSu\PyrusClient\Entity\Catalog\CatalogCreate;
@@ -29,17 +27,9 @@ final class PyrusGatewayImpl implements PyrusGateway
     /**
      * {@inheritdoc}
      */
-    public function useAuthToken(PyrusAuthToken $token): void
+    public function getClient(): PyrusClient
     {
-        $this->client->useAuthToken($token);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function useAuthCredentials(PyrusCredentials $credentials): void
-    {
-        $this->client->useAuthCredentials($credentials);
+        return $this->client;
     }
 
     /**
