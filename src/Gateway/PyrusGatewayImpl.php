4144982af97e6096c9065c8878e42e19ef32aa1c
marvin255 <postbox433@gmail.com>
2025-04-17 11:52:00 +0200

    Fix response parsing for getTask method (#25)

--- src/Gateway/PyrusGatewayImpl.php
+++ src/Gateway/PyrusGatewayImpl.php
@@ -167,7 +167,7 @@ final class PyrusGatewayImpl implements PyrusGateway
         );
 
         /** @var FormTask */
-        $task = $this->dataConverter->denormalize($raw, FormTask::class);
+        $task = $this->dataConverter->denormalize($raw['task'] ?? [], FormTask::class);
 
         return $task;
     }
